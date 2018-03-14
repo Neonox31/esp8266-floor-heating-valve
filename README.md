35e7af30b320f5ca9bc2eba329be7ed5923f854b
WEBER Logan <weberlogan@gmail.com>
2018-03-14 20:50:17 +0100

    feat(homie): upgrade to v2.0 version

---
+++ README.md
@@ -0,0 +1,19 @@
+# ESP8266 Heating Valve
+
+An Homie (https://github.com/marvinroger/homie-esp8266) based program to control homemade heating valves using a Wemos D1 mini, an ULN2003 chip, and a 28BYJ-48 stepper motor.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+This project is genrated with PlatformIO, so please install it before.
+
+### Installing
+
+```
+git clone https://github.com/Neonox31/esp8266-floor-heating-valve.git
+cd esp8266-floor-heating-valve
+pio init
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,20 @@
-#include "Stepper.h"
+#include <Stepper.h>
 #include <Homie.h>
 
 #define FW_NAME "heating-valve-firmware"
 #define FW_VERSION "1.0.0"
 
-/* Magic sequence for Autodetectable Binary Upload */
-const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
-const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
-/* End of magic sequence for Autodetectable Binary Upload */
-
 const int MOTOR_PINS[] = {D4, D5, D2, D1};
-const int REVOLUTION_STEPS = 2048; // One turn
-const int CYCLE_STEPS = (REVOLUTION_STEPS * 1) + (REVOLUTION_STEPS * 0.5); // One turn and a half
-const int INIT_CYCLE_STEPS = CYCLE_STEPS + REVOLUTION_STEPS; // 2 turns and a half
+const int MOTOR_SPEED = 4;
+const int MOTOR_REVOLUTION_STEPS = 2048;
+
+const int VALVE_CYCLE_STEPS = (MOTOR_REVOLUTION_STEPS * 1.5);
 
 int currentLevel = -1;
+int targetLevel = -1;
 
 Stepper motor(4096, MOTOR_PINS[0], MOTOR_PINS[2], MOTOR_PINS[1], MOTOR_PINS[3]);
-HomieNode valveNode("valve", "");
+HomieNode valveNode("valve", "valve");
 
 void resetMotorPins() {
   for (int i = 0; i < 4; ++i) {
@@ -25,51 +22,54 @@ void resetMotorPins() {
   }
 }
 
-void setLevel(int level) {
-  int targetLevel = 0;
-  int steps = 0;
-
-  if (currentLevel == -1 || level < 0 || level > 100) {
-    return;
-  }
-
-  targetLevel = currentLevel - level;
-  steps = (int) ((CYCLE_STEPS * targetLevel) / 100);
-  Serial.print("Move motor by ");
-  Serial.print(steps);
-  Serial.print(" steps (");
-  Serial.print(level);
-  Serial.println("%)...");
-  motor.step(steps);
-  currentLevel = level;
-  Homie.setNodeProperty(valveNode, "level", String(level), true);
-  resetMotorPins();
+void initMotor() {
+  Homie.getLogger() << "init motor speed and position..." << endl;
+  motor.setSpeed(MOTOR_SPEED);
+  motor.step(VALVE_CYCLE_STEPS + MOTOR_REVOLUTION_STEPS);
+  currentLevel = 0;
+  targetLevel = 100;
 }
 
-bool valveStateHandler(String level) {
-  setLevel(level.toInt());
+void setLevel(int level) {
+  int steps = 0;
+
+  if (level != currentLevel) {
+    steps = (int) ((VALVE_CYCLE_STEPS * (currentLevel - level)) / 100);
+    Homie.getLogger() << "move motor by " << steps << " steps (" << level << "%)..." << endl;
+    motor.step(steps);
+    resetMotorPins();
+    currentLevel = level;
+    valveNode.setProperty("level").send(String(currentLevel));
+  }
+}
+
+bool valveLevelHandler(const HomieRange& range, const String& value) {
+  if (value.toInt() < 0 || value.toInt() > 100) {
+    return false;
+  }
+  targetLevel = value.toInt();
   return true;
 }
 
-void initMotor() {
-  Serial.println("Init motor position...");
-  motor.step(INIT_CYCLE_STEPS);
-  currentLevel = 0;
-  setLevel(100);
+void setupHandler() {
+  initMotor();
+  valveNode.setProperty("unit").send("%");
 }
 
-void setupHandler() {
-  Homie.setNodeProperty(valveNode, "unit", "%", true);
+void loopHandler() {
+  setLevel(targetLevel);
 }
 
 void setup() {
-  motor.setSpeed(4);
-  initMotor();
+  Serial.begin(115200);
+  Serial << endl << endl;
 
-  Homie.setFirmware(FW_NAME, FW_VERSION);
-  valveNode.subscribe("level", valveStateHandler);
-  Homie.registerNode(valveNode);
-  Homie.setSetupFunction(setupHandler);
+  Homie_setFirmware(FW_NAME, FW_VERSION);
+  Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
+  
+  valveNode.advertise("unit");
+  valveNode.advertise("level").settable(valveLevelHandler);
+
   Homie.setup();
 }
 
